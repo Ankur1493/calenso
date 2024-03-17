d8c37d7af621af588da251b251be86f89f9903e6
Ankur1493 <ankursharma1493@gmail.com>
2024-03-18 01:51:24 +0530

    initialized the application

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+app.use(cors(corsOptions))
+const URI = process.env.MONGO_DB_URI as string;
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (_: Request, res: Response) => {
+  res.status(200).json({ status: "all clear" })
+});
+
+mongoose.connect(URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    });
+  }).catch(_ => {
+    console.error("error occured during db connection")
+  })
