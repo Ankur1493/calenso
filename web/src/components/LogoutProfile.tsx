f65be80bc4cc6568d7960dedd846f807de80b0cc
Ankur1493 <ankursharma1493@gmail.com>
2024-03-23 20:03:51 +0530

    user logout button ui

---
+++ web/src/components/LogoutProfile.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const LogoutProfile = ({ username }: { username: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleLogout = () => {
+    console.log("handle logout")
+  }
+  return (
+    <div className="relative">
+      <button
+        className="bg-second text-white font-bold py-2 px-6 rounded-[20px] text-[20px] inline-flex items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{username}</span>
+        <svg
+          className="ml-2 w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            <a
+              href="#profile"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Profile
+            </a>
+            <a
+              href="#logout"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            ><button onClick={handleLogout}>
+                Logout
+              </button>
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LogoutProfile;
+
--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
 import github from "../assets/icons/github.png"
 import calender from "../assets/images/landing-cal-1.jpeg"
-import {GridLayout} from "../components/GridLayout"
+import { GridLayout } from "../components/GridLayout"
 import { ScrollDiv } from "../components/ScrollDiv"
+import { useSelector } from "react-redux"
+import { RootState } from "@reduxjs/toolkit/query"
+import LogoutProfile from "../components/LogoutProfile"
 
 function Landing() {
+
+  const { userInfo } = useSelector((state: RootState) => state.auth)
+
   return (
     <div>
       <div className='flex justify-between px-16 py-10'>
@@ -24,11 +30,15 @@ function Landing() {
               </div>
             </a>
           </div>
-          <div className=' flex items-center bg-main rounded-3xl w-36 h-11 px-1.5 border-mainText border-2 text-mainText  hover:text-main hover:bg-mainText'>
-            <Link to={"/login"} className='cursos-pointer w-full flex justify-center'>
-              <p className='font-heading font-semibold text-2xl '>Login</p>
-            </Link>
-          </div>
+          {userInfo ? (
+            <LogoutProfile username={userInfo.username} />
+          ) : (
+            <div className=' flex items-center bg-main rounded-3xl w-36 h-11 px-1.5 border-mainText border-2 text-mainText  hover:text-main hover:bg-mainText'>
+              <Link to={"/login"} className='cursos-pointer w-full flex justify-center'>
+                <p className='font-heading font-semibold text-2xl '>Login</p>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
       <div className='flex flex-row justify-center px-16 mt-20'>
@@ -47,7 +57,7 @@ function Landing() {
             </div>
           </div>
         </div>
-        <div className='mr-16 w-6/12 h-5/12'><img src={calender} alt="" className="rounded-3xl w-full"/>
+        <div className='mr-16 w-6/12 h-5/12'><img src={calender} alt="" className="rounded-3xl w-full" />
         </div>
       </div>
       <div className='px-32 w-full'>
@@ -55,19 +65,19 @@ function Landing() {
       </div>
       <div className="flex flex-row justify-between px-16 mt-24 mx-28">
         <div className="flex flex-row w-11/12">
-            <div className="w-3/12 p-8 bg-secondText border-second border-r-2 border-dashed rounded-s-3xl">
-                <div className="flex items-center h-full"><a href="/"><p className="font-secondHeading text-2xl">Calenso.com/</p></a></div>
-            </div>
-            <div className="w-10/12 h-full rounded-e-3xl p-8 bg-input">
-                <div className="flex items-center h-full"><input type="text" placeholder="Calenso123" className="border-none decoration-none w-full text-2xl bg-transparent focus:outline-none"/></div>
-            </div>
+          <div className="w-3/12 p-8 bg-secondText border-second border-r-2 border-dashed rounded-s-3xl">
+            <div className="flex items-center h-full"><a href="/"><p className="font-secondHeading text-2xl">Calenso.com/</p></a></div>
+          </div>
+          <div className="w-10/12 h-full rounded-e-3xl p-8 bg-input">
+            <div className="flex items-center h-full"><input type="text" placeholder="Calenso123" className="border-none decoration-none w-full text-2xl bg-transparent focus:outline-none" /></div>
+          </div>
         </div>
         <div className="w-4/12 flex justify-center items-center">
-            <button className="flex items-center justify-center p-6 w-8/12 bg-second rounded-3xl">
-                <div className="text-mainText text-3xl font-secondHeading">
-                    <p className="mb-2">Claim</p><p> username</p>
-                </div>
-            </button>
+          <button className="flex items-center justify-center p-6 w-8/12 bg-second rounded-3xl">
+            <div className="text-mainText text-3xl font-secondHeading">
+              <p className="mb-2">Claim</p><p> username</p>
+            </div>
+          </button>
         </div>
       </div>
       <div className="p-16">
@@ -75,7 +85,7 @@ function Landing() {
       </div>
       <div className="flex justify-center">
         <div className="w-7/12 text-center">
-            <p className="font-secondHeading text-mainText text-7xl font-bold">Everything you need in a scheduling app</p>
+          <p className="font-secondHeading text-mainText text-7xl font-bold">Everything you need in a scheduling app</p>
         </div>
       </div>
       <div className="mt-32">
@@ -83,11 +93,11 @@ function Landing() {
       </div>
       <div className="flex justify-center mt-32">
         <div className="w-7/12 text-center">
-            <p className="font-secondHeading text-mainText text-6xl font-bold">Tailored scheduling for every business, every time</p>
+          <p className="font-secondHeading text-mainText text-6xl font-bold">Tailored scheduling for every business, every time</p>
         </div>
       </div>
       <div className="flex items-center justify-center mt-32 mb-16">
-        <div className='w-8xl'><img src={calender} alt="" className="rounded-3xl w-full"/></div>
+        <div className='w-8xl'><img src={calender} alt="" className="rounded-3xl w-full" /></div>
       </div>
 
     </div>
