0d2017acf940aa4b16ff6d4eec59b5ad0a674b1f
Ankur1493 <ankursharma1493@gmail.com>
2024-03-29 04:14:09 +0530

    added the delete and details queries

--- web/src/slices/meetingsApiSlice.ts
+++ web/src/slices/meetingsApiSlice.ts
@@ -1,16 +1,29 @@
-// import { MEETINGS_URL } from "../constants/constants";
+import { MEETINGS_URL } from "../constants/constants";
 import { apiSlice } from "./apiSlice";
 
 export const meetingsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     meetings: builder.query({
       query: () => ({
-        url: `meetings/all`,
+        url: `${MEETINGS_URL}/all`,
         method: "GET",
       }),
       keepUnusedDataFor: 5,
     }),
+    meetingDetails: builder.query({
+      query: (id) => ({
+        url: `${MEETINGS_URL}/${id}`,
+        method: "GET",
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    deleteMeeting: builder.mutation({
+      query: (id) => ({
+        url: `${MEETINGS_URL}/${id}`,
+        method: "DELETE"
+      })
+    })
   }),
 });
 
-export const { useMeetingsQuery } = meetingsApiSlice;
+export const { useMeetingsQuery, useMeetingDetailsQuery, useDeleteMeetingMutation } = meetingsApiSlice;
