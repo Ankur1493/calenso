b7920985b97132472cd0e4d011861eda51fd7ef0
Udit Kapoor <uditkapoor060@gmail.com>
2024-04-14 00:30:44 +0530

    changed ui of the calendar

--- web/src/components/Calendar.tsx
+++ web/src/components/Calendar.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import "react-calendar/dist/Calendar.css"; // Import Calendar CSS
+import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
+import { isSameMonth } from "date-fns";
 
 const CalendarComponent = () => {
   const [date, setDate] = useState(new Date());
 
   const onChange = (newDate) => {
     setDate(newDate);
+  };
+
+  const tileClassName = ({ date, view }) => {
+    return view === "month" && isSameMonth(date, new Date());
   };
 
   return (
@@ -33,15 +38,26 @@ const CalendarComponent = () => {
             font-family: 'Poppins', sans-serif;
           }
           
+          .react-calendar__navigation__label:hover{
+            background: transparent !important;
+          }
+
+          .react-calendar__navigation__arrow:hover{
+            background: input !important;
+            opacity: 0.4;
+            color: black;
+            border-radius: 5px;
+          }
+
           .react-calendar__tile {
             background: transparent;
-            color: #000000; /* Color of available dates */
             transition: opacity 0.2s ease;
-            border: none; /* Remove border from tiles */
+            border: none;
+            color: #fff
           }
           
           .react-calendar__tile--active {
-            background: #1E1F20; /* Second color for active dates */
+            background: #1E1F20; 
           }
           
           .react-calendar__tile--hasActive {
@@ -49,11 +65,21 @@ const CalendarComponent = () => {
           }
           
           .react-calendar__tile:hover {
-            border: none; /* No border on hover */
+            border: none; 
+            background: input !important;
+            opacity:0.4;
+            color: black;
+            border-radius: 5px
           }
           
-          .react-calendar__month-view__days__day {
-            color: #FFFFFF; /* Color of the days */
+          .text-white {
+            color: #FFFFFF; 
+          }
+
+          .react-calendar__month-view__weekdays__weekday {
+            color: #ffffff;
+            opacity: 0.7;
+            padding-bottom: 20px;
           }
         `}
       </style>
