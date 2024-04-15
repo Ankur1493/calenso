74252c866ed8f1ab61e39895ec0dd3b0ffbc4ee2
Ankur1493 <ankursharma1493@gmail.com>
2024-04-16 05:23:48 +0530

    modified the ui

---
+++ web/src/Hooks/useTruncateDescription.tsx
@@ -0,0 +1,10 @@
+import { useMemo } from 'react';
+
+function useTruncatedDescription(description: string) {
+  return useMemo(() => {
+    return description.length > 75 ? `${description.substring(0, 75)}...` : description;
+  }, [description]);
+}
+
+export default useTruncatedDescription;
+
--- web/src/components/DisplayMeetingCard.tsx
+++ web/src/components/DisplayMeetingCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import useTruncatedDescription from '../Hooks/useTruncateDescription';
 
 interface props {
   id: string;
@@ -9,18 +10,17 @@ interface props {
 }
 
 const DisplayMeetingCard = ({ id, title, duration, info, username }: props) => {
+  const truncatedInfo = useTruncatedDescription(info);
+
   return (
-    <div
-      className=" h-[100px] border border-gray-400 px-4 my-2 rounded-lg w-8/12 bg-second text-white cursor-pointer"
-      key={id}
-    >
+    <div className="lg:h-[100px] h-[150px] border border-gray-400 px-4 my-2 rounded-lg w-10/12 sm:w-8/12 bg-second text-white cursor-pointer" key={id}>
       <Link to={`/${username}/${id}`}>
         <div className="flex flex-col justify-around h-full py-2">
           <div className="flex w-full justify-between">
-            <div className="text-2xl md:text-3xl">{title}</div>
+            <div className="text-3xl">{title}</div>
             <div className="text-gray-400">{duration} minutes</div>
           </div>
-          <div>{info}</div>
+          <div className="w-3/4">{truncatedInfo}</div>
         </div>
       </Link>
     </div>
@@ -28,3 +28,4 @@ const DisplayMeetingCard = ({ id, title, duration, info, username }: props) => {
 };
 
 export default DisplayMeetingCard;
+
