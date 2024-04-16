ff9c916ede0877fac6367d69230e578f0c5ae543
Udit Kapoor <uditkapoor060@gmail.com>
2024-04-16 15:08:07 +0530

    Make the Booking page and event page responsive

--- web/src/components/Bookings.tsx
+++ web/src/components/Bookings.tsx
@@ -27,7 +27,7 @@ function Bookings() {
           <div className="flex flex-col">
             <div className="flex flex-row flex-wrap justify-between">
               <div className="mb-4 h-9 max-w-full lg:mb-5">
-                <nav className="flex max-h-9 space-x-12 rounded-md">
+                <nav className="flex max-h-9 space-x-4 lg:space-x-12 rounded-md">
                   <button
                     className={`bg-emphasis text-input font-heading inline-flex items-center justify-center md:mb-0 px-4 mx-4  py-2.5 text-md ${
                       filter === "upcoming"
