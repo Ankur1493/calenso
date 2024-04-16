e5cb91b20f8410fa6b8c66bac594c64226bbf98e
Udit Kapoor <uditkapoor060@gmail.com>
2024-04-16 19:30:19 +0530

    Make the booking details responsive

--- web/src/pages/BookingDetails.tsx
+++ web/src/pages/BookingDetails.tsx
@@ -39,8 +39,8 @@ const BookingDetails = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <div className="flex flex-col items-center py-5 bg-home border rounded-[20px] border-gray-400 w-1/3 text-white">
+    <div className="flex justify-center items-center h-screen overflow-y-auto w-screen">
+      <div className="flex flex-col items-center py-5 bg-home border rounded-[20px] border-gray-400 w-full md:w-1/3 text-white">
         <span
           className="flex justify-start w-full px-4 cursor-pointer font-heading font-semibold opacity-80"
           onClick={() => navigate(-1)}
@@ -50,7 +50,7 @@ const BookingDetails = () => {
         <div className="text-3xl font-semibold mb-8 text-secondHeading">
           {Booking.title}
         </div>
-        <div className="flex w-3/4 border-b border-b-gray-500 pb-4 justify-between">
+        <div className="flex flex-col lg:flex-row w-3/4 border-b border-b-gray-500 pb-4 sm:justify-between">
           <div className="text-[18px] font-heading">Attendees - </div>
           <div className="text-gray-300 text-right font-heading text-sm">
             <p className="mb-3">{Booking.guestUser}</p>
@@ -98,16 +98,16 @@ const BookingDetails = () => {
             {Booking.description}
           </div>
         </div>
-        <div className="mt-4 pb-4 text-[25px] text-gray-500 font-heading">
+        <div className="mt-4 pb-4 text-[25px] text-center text-gray-500 font-heading">
           <div>{Booking && Booking.canceled ? "canceled" : startingIn}</div>
         </div>
         {Booking && !Booking.canceled && (
-          <div className="w-3/4 flex justify-between mt-4">
+          <div className="w-full sm:w-3/4 flex flex-col justify-center items-center sm:flex-row sm:justify-between mt-4 space-y-4 sm:space-y-0">
             <a
               href={Booking.event.meetLink}
               target="_blank"
               className={`${
-                !isOccured ? "w-full" : "w-1/3"
+                !isOccured ? "w-3/4" : " w-3/4 md:w-1/3"
               } flex justify-center py-2 rounded-lg font-heading hover:bg-gray-500 hover:text-black duration-200 border-gray-500 border`}
             >
               Join Event
@@ -115,7 +115,7 @@ const BookingDetails = () => {
             {isOccured && (
               <button
                 onClick={cancelBooking}
-                className="w-1/3 flex justify-center font-heading py-2 rounded-lg bg-mainText text-black hover:bg-home hover:text-white  duration-200 border-gray-500 border"
+                className="w-3/4 md:w-1/3 flex justify-center font-heading py-2 rounded-lg bg-mainText text-black hover:bg-home hover:text-white duration-200 border-gray-500 border"
               >
                 Cancel Event
               </button>
